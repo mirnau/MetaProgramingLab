d7ba534bfccb852b5ab9093537cd18e4f99ba048
Mikael Bernau <79576604+mbernau83@users.noreply.github.com>
2023-02-17 11:48:06 +0100

    Labbhjälp

--- MetaProgrammingLab/Add.hpp
+++ MetaProgrammingLab/Add.hpp
@@ -1,19 +1,36 @@
 #pragma once
-template <int x, int y>
+#include <type_traits>
 
+static int AddFun(int x, int y)
+{
+	if (x == 0)
+	{
+		return y;
+	}
+	else
+	{
+		return AddFun(x - 1, y + 1);
+	}
+}
+
+template <int x, int y>
 struct Add
 {
-	int AddFun(int x, int y)
-	{
-		if (x == 0)
-		{
-			return y;
-		}
-		else
-		{
-			return AddFun(x - 1, y + 1);
-		}
-	}
-
-	static const int value = Add<x - 1, y + 1>::value;
+	static const int value = Add<x - 1, y + 1>::value; 
 };
+
+template <int y>
+struct Add<0, y>
+{
+	static const int value = y;
+};
+
+//static void foo()
+//{
+//	static_assert(Add<3, 5>::value == 8);
+//
+//	int x2 = Add<3, 5>::value
+//
+//}
+
+
